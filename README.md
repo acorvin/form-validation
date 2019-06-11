d6f6e04a95aaa4e0187ca5d77a634c3532c3b86b
acorvin <48366916+acorvin@users.noreply.github.com>
2019-06-10 21:41:29 -0600

    initial commit
    Initial commit of form-validation project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # form-validation
+
+A simple form validation using both Ajax and jQuery.
+
+Alerts are generated upon submit for invalid values as well as missing field inputs.
---
+++ script.js
@@ -0,0 +1,45 @@
+// Email validation
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
+
+// Submit Button
+$('#submitButton').click(function() {
+  var errorMessage = '';
+  var fieldsMissing = '';
+  // Missing fields error check
+  if ($('#email').val() == '') {
+    fieldsMissing += '<p>Email</p>';
+  }
+  if ($('#phone').val() == '') {
+    fieldsMissing += '<p>Phone</p>';
+  }
+  if ($('#password').val() == '') {
+    fieldsMissing += '<p>Password</p>';
+  }
+  if ($('#passwordConfirm').val() == '') {
+    fieldsMissing += '<p>Confirm Password</p>';
+  }
+  // Missing field error alert
+  if (fieldsMissing != '') {
+    errorMessage += '<p>The following filds are missing</p>' + fieldsMissing;
+  }
+  // Validation checks
+  if (isEmail($('#email').val()) == false && fieldsMissing == false) {
+    errorMessage += '<p>Invalid email address</p>';
+  }
+  if ($.isNumeric($('#phone').val()) == false && fieldsMissing == false) {
+    errorMessage += '<p>Non-numeric phone number</p>';
+  }
+  if ($('#password').val() != $('#passwordConfirm').val()) {
+    errorMessage += '<p>Mismatched password</p>';
+  }
+  // Success / Error alert
+  if (errorMessage != '') {
+    $('#errorMessage').html(errorMessage);
+  } else {
+    $('#successMessage').show();
+    $('#errorMessage').hide();
+  }
+});
